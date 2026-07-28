d9421052a94e458faa61d520040599c90134a0f6
Stanley Yin <stanleyy@andrew.cmu.edu>
2026-07-28 15:23:16 -0400

    Tweak for 8x8

--- accelerated/device/rtl/systolic_array.sv
+++ accelerated/device/rtl/systolic_array.sv
@@ -11,7 +11,7 @@ module systolic_array (
     output logic [ACC_WIDTH-1:0] acc_values   [0:ARRAY_DIM-1][0:ARRAY_DIM-1]
 );
 
-    localparam ARRAY_DIM = 4;
+    localparam ARRAY_DIM = 8;
     localparam MUL_WIDTH = 16;
     localparam ACC_WIDTH = 48;
 
